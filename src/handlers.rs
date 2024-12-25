c712e022a9f87a12028320028da80114f6808abe
Mauro Garcia <main@mgc.sh>
2024-12-25 14:56:46 -0300

    it compiles but there's some bugs to fix later

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,60 +1,12 @@
-use crate::templates::{build_blog_post, BlogPost, BlogPostsListTemplate};
+use crate::templates::{BlogPostTemplate, BlogPostPreview, BlogPostsListTemplate};
 
-use std::fs::read_dir;
+use std::{fs::{read_dir, DirEntry, File, ReadDir}, io::{Error, ErrorKind, Read}};
 
 use axum::{extract::Path, http::StatusCode, response::{IntoResponse, Response}};
 use std::path::PathBuf;
-use tracing::info;
+use tracing::{info, error};
 
-pub async fn handle_yearly_blogs(Path(year): Path<u16>) -> Response {
-    info!("year recieved: {year}");
-    let base_dir = PathBuf::from("static/blog/posts/");
-    let year_dir = base_dir.join(year.to_string());
-
-    let year_string = year_dir.clone().into_os_string().into_string().ok().unwrap();
-    // if only year is some, and year exists in dir, show page with all blog posts
-
-    if year_dir.exists() {
-        let month_dirs = read_dir(year_dir).unwrap(); // only shows the directory itself. not the files it contains
-        info!("file {year_string} exists");
-        
-        // get some real blogposts?
-        let blog_posts: Vec<BlogPost> = vec![
-            // todo remove this sample
-            build_blog_post(
-                "posts/2023/july/handwired-corne_en.html".to_string(),
-                "handwired corne".to_string(),
-                "storytime | build".to_string(),
-                "15-Jul-2023".to_string(),
-                "yeah".to_string()),
-            ];
-
-        for value in month_dirs {
-            let contains = read_dir(value.unwrap().path());
-
-            if contains.is_ok() {
-                info!("dirs {}", contains.unwrap().next().unwrap().unwrap().path().display());
-            }
-            else {
-                info!("no files inside");
-            }
-
-
-            
-        }
-
-        let body = BlogPostsListTemplate {
-            blog_posts
-        };
-        body.into_response()
-
-    } else {
-        info!("uhh {year_string} does not exist");
-        (StatusCode::NOT_FOUND, format!("No blog posts found for year: {year}")).into_response()
-    }
-}
-
-pub async fn handle_monthly_blogs(Path((year, month)): Path<(u16, u8)>) -> Response {
+pub async fn handle_monthly_blogs(Path((year, month)): Path<(u16, Option<u8>)>) -> Response {
     info!("year recieved: {year}");
     let base_dir: PathBuf = PathBuf::from("static/blog/posts/");
     let year_dir: PathBuf = base_dir.join(year.to_string());
@@ -64,36 +16,52 @@ pub async fn handle_monthly_blogs(Path((year, month)): Path<(u16, u8)>) -> Respo
 
     if year_dir.exists() {
         info!("dir {year_string} exists");
+        
+        let month_dir: PathBuf;
+        let month_string: String;
+        // if month.is_some() {
+            month_dir = year_dir.join(month.unwrap().to_string());
+            month_string = month_dir.clone().into_os_string().into_string().ok().unwrap();
+        // } else {
+        //     // return template for yearly blog posts
+        // }
+    
+        // get some real blog posts?
+        let mut blog_posts: Vec<BlogPostPreview> = vec![];
+                
 
-        let month_dir: PathBuf = year_dir.join(month.to_string());
-        let month_string: String = month_dir.clone().into_os_string().into_string().ok().unwrap();
         if month_dir.exists() {
             info!("dir {month_string} exists");
             
-            // prints all directories available, should turn this data into a vector of blogposts
-            for path in month_dir.read_dir().unwrap() {
-                println!("Name: {}", path.unwrap().path().display())
-            }
+            // prints all directories available, should turn this data into a vector of blog posts
+            let files = get_all_files_in_dir(month_dir).unwrap();
+            for path in files {
+                let p = path.path();
+                println!("Name: {}", p.display());
 
-            // get some real blogposts?
-            let blog_posts: Vec<BlogPost> = vec![
-                build_blog_post(
-                    "posts/2023/july/handwired-corne_en.html".to_string(),
-                    "handwired corne".to_string(),
-                    "storytime | build".to_string(),
-                    "15-Jul-2023".to_string(),
-                    "yeah".to_string()),
-                ];
-        
-            let body = BlogPostsListTemplate {
+                // read the file into a string and send it
+                let mut filestr= String::new();
+                if File::open(path.path()).unwrap().read_to_string(&mut filestr).is_err() {
+                    error!("Could not open file");
+                } else {
+                    blog_posts.push(
+                        BlogPostTemplate::from_file(
+                            p.into_os_string().into_string().unwrap(),
+                            &filestr).unwrap().preview
+                            // get the preview value for each blog post, since this is the monthly list of blog posts
+                    );
+                }              
+            }
+            info!("blog post {:?}", blog_posts);
+            let body: BlogPostsListTemplate = BlogPostsListTemplate {
                 blog_posts
             };
 
             body.into_response()
-            
+
         } else {
             info!("uhh {month_string} does not exist");
-            (StatusCode::NOT_FOUND, format!("No blog posts found for month: {month}")).into_response()
+            (StatusCode::NOT_FOUND, format!("No blog posts found for month: {}", month.unwrap())).into_response()
         }
     } else {
         info!("uhh {year_string} does not exist");
@@ -117,14 +85,14 @@ pub async fn handle_blog_post(Path((year, month, post)): Path<(u16, u8, String)>
         if month_dir.exists() {
             info!("dir {month_string} exists");
             
-            // prints all directories available, should turn this data into a vector of blogposts
+            // prints all directories available, should turn this data into a vector of blog posts
             for path in month_dir.read_dir().unwrap() {
                 println!("Name: {}", path.unwrap().path().display())
             }
 
             // get some real blogposts?
-            let blog_posts: Vec<BlogPost> = vec![
-                build_blog_post(
+            let blog_posts: Vec<BlogPostPreview> = vec![
+                BlogPostPreview::from_params(
                     "posts/2023/july/handwired-corne_en.html".to_string(),
                     "handwired corne".to_string(),
                     "storytime | build".to_string(),
@@ -178,3 +146,114 @@ pub async fn handle_blog_post(Path((year, month, post)): Path<(u16, u8, String)>
 //             (axum::http::StatusCode::NOT_FOUND, "File not found".to_string())
 //         }
 // }
+
+pub async fn handle_yearly_blogs(Path(year): Path<u16>) -> Response {
+    info!("year recieved: {year}");
+    let base_dir = PathBuf::from("static/blog/posts/");
+    let year_dir = base_dir.join(year.to_string());
+
+    let year_string = year_dir.clone().into_os_string().into_string().ok().unwrap();
+    // if only year is some, and year exists in dir, show page with all blog posts
+
+    if year_dir.exists() {
+        let month_dirs = read_dir(year_dir).unwrap(); // only shows the directory itself. not the files it contains
+        info!("file {year_string} exists");
+        
+        // get some real blogposts?
+        let blog_posts: Vec<BlogPostPreview> = vec![
+            // todo remove this sample
+            BlogPostPreview::from_params(
+                "posts/2023/july/handwired-corne_en.html".to_string(),
+                "handwired corne".to_string(),
+                "storytime | build".to_string(),
+                "15-Jul-2023".to_string(),
+                "yeah".to_string()),
+        ];
+
+        for value in month_dirs {
+            let contains = read_dir(value.unwrap().path());
+
+            if contains.is_ok() {
+                info!("dirs {}", contains.unwrap().next().unwrap().unwrap().path().display());
+            }
+            else {
+                info!("no files inside");
+            }
+        }
+
+        let body = BlogPostsListTemplate {
+            blog_posts
+        };
+        body.into_response()
+
+    } else {
+        info!("uhh {year_string} does not exist");
+        (StatusCode::NOT_FOUND, format!("No blog posts found for year: {year}")).into_response()
+    }
+}
+
+fn get_all_files_in_dir(directory: PathBuf) -> Result<Vec<DirEntry>, Error> {
+    let mut entries = Vec::new();
+
+    match directory.read_dir() {
+        Ok(dir_iter) => {
+            for entry in dir_iter {
+                match entry {
+                    Ok(entry) => {
+                        match entry.file_type() {
+                            Ok(file_type) => {
+                                if file_type.is_file() {
+                                    entries.push(entry);
+                                }
+                            }
+                            Err(e) => 
+                                return Err(
+                                    Error::new(
+                                        ErrorKind::Other, 
+                                        format_and_log("Failed to check file type", Some(e)
+                                    ))
+                                ),
+                        }
+                    }
+                    Err(e) => match e.kind() {
+                        ErrorKind::NotFound => return Err(
+                            Error::new(ErrorKind::NotFound, 
+                                format_and_log("Directory not found", Some(e))
+                            )),
+                        ErrorKind::PermissionDenied => return Err(
+                            Error::new(ErrorKind::PermissionDenied, 
+                                format_and_log("Permission denied", None))),
+                        _ => return Err(
+                            Error::new(ErrorKind::Other, 
+                                format_and_log("Failed to read directory entry", Some(e)))),
+                    },
+                }
+            }
+            Ok(entries)
+        },
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => Err(
+                Error::new(ErrorKind::NotFound, 
+                    format_and_log("Directory not found", None)
+                    )),
+            ErrorKind::PermissionDenied => Err(
+                Error::new(ErrorKind::PermissionDenied, 
+                    format_and_log("Permission denied", None)
+                    )),
+            _ => Err(
+                Error::new(ErrorKind::Other, 
+                    format_and_log("Failed to read directory", Some(e))
+                )),
+        },
+    }
+}
+
+fn format_and_log(msg: &str, e: Option<Error>) -> String {
+    let err = if e.is_some() {
+        format!("{}: {}", msg, e.unwrap())
+    } else {
+        format!("{}", msg)
+    };
+    error!(err);
+    return err
+}
--- src/templates.rs
+++ src/templates.rs
@@ -1,12 +1,14 @@
 use askama_axum::Template;
+use std::error::Error;
 
 #[derive(Template)]
 #[template(path = "blog_posts_page_template.html")]
 pub struct BlogPostsListTemplate {
-    pub blog_posts: Vec<BlogPost>, 
+    pub blog_posts: Vec<BlogPostPreview>, 
 }
 
-pub struct BlogPost {
+#[derive(Debug)]
+pub struct BlogPostPreview {
     href: String,
     title: String,
     tags: String,
@@ -14,13 +16,91 @@ pub struct BlogPost {
     image: String,
 }
 
-pub fn build_blog_post(href: String, title: String, tags: String , date: String, image: String) -> BlogPost {
-    BlogPost {
-        href,
-        title,
-        tags,
-        date,
-        image
+impl BlogPostPreview{
+    pub fn from_params(href: String, title: String, tags: String, date: String, image: String) -> BlogPostPreview {
+
+        BlogPostPreview {
+            href,
+            title,
+            tags,
+            date,
+            image
+        }
     }
 }
 
+#[derive(Template)]
+#[template(path = "blog_post_template.html")]
+pub struct BlogPostTemplate {
+    pub preview: BlogPostPreview,
+    title: String,
+    subtitle: String,
+    header_title: String,
+    header_subtitle: String,
+    header_date: String,
+    indexes: Vec<String>,
+    content: String,
+}
+
+impl BlogPostTemplate {
+    fn from_params(preview: BlogPostPreview, title: String, subtitle: String, header_title: String, header_subtitle: String, header_date: String, 
+        indexes: Vec<String>, content: String) -> BlogPostTemplate {
+
+        BlogPostTemplate{
+            preview, 
+            title,
+            subtitle,
+            header_title, 
+            header_subtitle,
+            header_date, 
+            indexes, 
+            content
+        }
+    }
+
+    pub fn from_file(href: String, file_content: &str) -> Result<Self, Box<dyn Error>> {
+
+        let mut title: String = String::new();
+        let mut subtitle: String = String::new();
+        let mut tags: String = String::new();
+        let mut date: String = String::new();
+        let mut image: String = String::new();
+    
+        let mut header_subtitle: String = String::new();
+        let mut header_title: String = String::new();
+        let mut header_date: String = String::new();
+    
+        let mut indexes: Vec<String> = Vec::new();
+    
+        let mut post_content: String = String::new();
+    
+        let mut current_field = "";
+    
+        for line in file_content.lines() {
+            if line.starts_with("_") {
+                current_field = &line[1..];
+                if let Some((_, value)) = line.split_once("=") {
+                    match current_field {
+                        "title" => title = value.to_string(),
+                        "subtitle" => subtitle = value.to_string(),
+                        "tags" => tags = value.to_string(),
+                        "date" => date = value.to_string(),
+                        "image" => image = value.to_string(),
+                        "header_title" => header_title = value.to_string(), 
+                        "header_subtitle" => header_subtitle = value.to_string(),
+                        "header_date" => header_date = value.to_string(),
+                        "indexes" => indexes = value.split(',').map(|s| s.to_string()).collect(),
+                        _ => {}
+                    }
+                }
+            } else if current_field == "content" {
+                post_content.push_str(line);
+                post_content.push('\n'); // adding newline to preserve md format
+            }
+        }
+    
+        Ok(BlogPostTemplate::from_params(
+            BlogPostPreview::from_params(href, title.clone(), tags, date, image),
+            title, subtitle, header_title, header_subtitle, header_date, indexes, post_content))
+    }
+}
